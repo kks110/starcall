8ccc2ae168cc2f8ca15dcf7e427ea6f52d22ecb6
Kelvin Samuel <kelvin.samuel@simplybusiness.co.uk>
2020-06-25 10:12:50 +0100

    Change to class methods

--- lib/starcall/static_data.rb
+++ lib/starcall/static_data.rb
@@ -8,29 +8,29 @@ module Starcall
   class StaticData
     # Season ids are used in match history to indicate which season a match was played.
     # A full list of season ids can be retrieved with the below method.
-    def season_ids
+    def self.season_ids
       ApiRequests.make_request(url: static_url(search_term: 'seasons'))
     end
 
     # Queue ids show up in several places throughout the API and are used to indicate which kind of match was played.
     # A full list of queue ids can be can be retrieved with the below method.
-    def queue_ids
+    def self.queue_ids
       ApiRequests.make_request(url: static_url(search_term: 'queues'))
     end
 
     # Map ids are used in match history to indicate which map a match was played.
     # A full list of map ids can be can be retrieved with the below method.
-    def map_ids
+    def self.map_ids
       ApiRequests.make_request(url: static_url(search_term: 'maps'))
     end
 
     # A full list of game modes can be can be retrieved with the below method.
-    def game_modes
+    def self.game_modes
       ApiRequests.make_request(url: static_url(search_term: 'gameModes'))
     end
 
     # A full list of game types can be can be retrieved with the below method.
-    def game_types
+    def self.game_types
       ApiRequests.make_request(url: static_url(search_term: 'gameTypes'))
     end
 
@@ -39,75 +39,75 @@ module Starcall
     # however occasionally there will be additional builds.
     # This typically occurs when there's an error in the original build.
     # As such, you should always use the most recent Data Dragon version for a given patch for the best results.
-    def dd_versions
+    def self.dd_versions
       ApiRequests.make_request(url: dd_versions_url)
     end
 
     # Data Dragon versions aren't always equivalent to the League of Legends client version in a region.
     # You can find the version each region is using via the realms files.
-    def dd_euw_versions
+    def self.dd_euw_versions
       ApiRequests.make_request(url: dd_euw_specific_version_url)
     end
 
     # This returns a list of all champion with a brief summary, including stats, id and blurb.
-    def dd_champions
+    def self.dd_champions
       ApiRequests.make_request(url: dd_url(game_component: 'champion'))
     end
 
     # For me detailed and specific information about a champion, this call can be used.
-    def dd_specific_champion(champion_name:)
+    def self.dd_specific_champion(champion_name:)
       ApiRequests.make_request(url: dd_specific_champion_url(champion_name: champion_name))
     end
 
     # Data Dragon also provides detail for every item in the game.
     # with this method you can find info such as the item's description, purchase value, sell value,
     # items it builds from, items it builds into, and stats granted from the item.
-    def dd_items
+    def self.dd_items
       ApiRequests.make_request(url: dd_url(game_component: 'item'))
     end
 
     # Details about summoner spells.
-    def dd_summoner_spells
+    def self.dd_summoner_spells
       ApiRequests.make_request(url: dd_url(game_component: 'summoner'))
     end
 
     # Details about profile icons and where they can be found on the sprite sheets.
-    def dd_profile_icons
+    def self.dd_profile_icons
       ApiRequests.make_request(url: dd_url(game_component: 'profileicon'))
     end
 
     private
 
     # This help build the static data url for thins such as map ids.
-    def static_url(search_term:)
+    def self.static_url(search_term:)
       "http://static.developer.riotgames.com/docs/lol/#{search_term}.json"
     end
 
     # This is the data dragon version URL
-    def dd_versions_url
+    def self.dd_versions_url
       'https://ddragon.leagueoflegends.com/api/versions.json'
     end
 
     # This is the specific EUW data dragon version URL.
-    def dd_euw_specific_version_url
+    def self.dd_euw_specific_version_url
       'https://ddragon.leagueoflegends.com/realms/euw.json'
     end
 
     # This gets the current version of specific game components, such as champions.
-    def dd_current_data_version(game_component:)
+    def self.dd_current_data_version(game_component:)
       ApiRequests.make_request(url: dd_euw_specific_version_url)['n'][game_component]
     end
 
     # This builds the data dragon url for specific components such as champions,
     # using the above method to get the version.
-    def dd_url(game_component:)
+    def self.dd_url(game_component:)
       'http://ddragon.leagueoflegends.com/cdn/'\
       "#{dd_current_data_version(game_component: game_component)}/data/en_GB/"\
       "#{game_component}.json"
     end
 
     # This builds the data dragon url for a specific champion.
-    def dd_specific_champion_url(champion_name:)
+    def self.dd_specific_champion_url(champion_name:)
       'http://ddragon.leagueoflegends.com/cdn/'\
       "#{dd_current_data_version(game_component: 'champion')}"\
       "/data/en_GB/champion/#{champion_name}.json"
--- spec/starcall/static_data_spec.rb
+++ spec/starcall/static_data_spec.rb
@@ -23,8 +23,8 @@ RSpec.describe Starcall::StaticData do
     expect(ApiRequests).to have_received(:make_request).with(url)
   end
 
-  describe '#season_ids' do
-    subject(:season_ids) { described_class.new.season_ids }
+  describe '.season_ids' do
+    subject(:season_ids) { described_class.season_ids }
     let(:url) do
       { url: 'http://static.developer.riotgames.com/docs/lol/seasons.json' }
     end
@@ -34,8 +34,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#queue_ids' do
-    subject(:queue_ids) { described_class.new.queue_ids }
+  describe '.queue_ids' do
+    subject(:queue_ids) { described_class.queue_ids }
     let(:url) do
       { url: 'http://static.developer.riotgames.com/docs/lol/queues.json' }
     end
@@ -45,8 +45,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#map_ids' do
-    subject(:map_ids) { described_class.new.map_ids }
+  describe '.map_ids' do
+    subject(:map_ids) { described_class.map_ids }
     let(:url) do
       { url: 'http://static.developer.riotgames.com/docs/lol/maps.json' }
     end
@@ -56,8 +56,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#game_modes' do
-    subject(:game_modes) { described_class.new.game_modes }
+  describe '.game_modes' do
+    subject(:game_modes) { described_class.game_modes }
     let(:url) do
       { url: 'http://static.developer.riotgames.com/docs/lol/gameModes.json' }
     end
@@ -67,8 +67,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#game_types' do
-    subject(:game_types) { described_class.new.game_types }
+  describe '.game_types' do
+    subject(:game_types) { described_class.game_types }
     let(:url) do
       { url: 'http://static.developer.riotgames.com/docs/lol/gameTypes.json' }
     end
@@ -78,8 +78,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#dd_versions' do
-    subject(:dd_versions) { described_class.new.dd_versions }
+  describe '.dd_versions' do
+    subject(:dd_versions) { described_class.dd_versions }
     let(:url) do
       { url: 'https://ddragon.leagueoflegends.com/api/versions.json' }
     end
@@ -89,8 +89,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#dd_euw_versions' do
-    subject(:dd_euw_versions) { described_class.new.dd_euw_versions }
+  describe '.dd_euw_versions' do
+    subject(:dd_euw_versions) { described_class.dd_euw_versions }
     let(:url) do
       { url: 'https://ddragon.leagueoflegends.com/realms/euw.json' }
     end
@@ -100,8 +100,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#dd_champions' do
-    subject(:dd_champions) { described_class.new.dd_champions }
+  describe '.dd_champions' do
+    subject(:dd_champions) { described_class.dd_champions }
     let(:url) do
       { url: "http://ddragon.leagueoflegends.com/cdn/#{response['n']['champion']}/data/en_GB/champion.json" }
     end
@@ -111,8 +111,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#dd_items' do
-    subject(:dd_items) { described_class.new.dd_items }
+  describe '.dd_items' do
+    subject(:dd_items) { described_class.dd_items }
     let(:url) do
       { url: "http://ddragon.leagueoflegends.com/cdn/#{response['n']['item']}/data/en_GB/item.json" }
     end
@@ -122,8 +122,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#dd_summoner_spells' do
-    subject(:dd_summoner_spells) { described_class.new.dd_summoner_spells }
+  describe '.dd_summoner_spells' do
+    subject(:dd_summoner_spells) { described_class.dd_summoner_spells }
     let(:url) do
       { url: "http://ddragon.leagueoflegends.com/cdn/#{response['n']['summoner']}/data/en_GB/summoner.json" }
     end
@@ -133,8 +133,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#dd_profile_icons' do
-    subject(:dd_profile_icons) { described_class.new.dd_profile_icons }
+  describe '.dd_profile_icons' do
+    subject(:dd_profile_icons) { described_class.dd_profile_icons }
     let(:url) do
       { url: "http://ddragon.leagueoflegends.com/cdn/#{response['n']['profileicon']}/data/en_GB/profileicon.json" }
     end
@@ -144,8 +144,8 @@ RSpec.describe Starcall::StaticData do
     end
   end
 
-  describe '#dd_specific_champion' do
-    subject(:dd_specific_champion) { described_class.new.dd_specific_champion(champion_name: champion_name) }
+  describe '.dd_specific_champion' do
+    subject(:dd_specific_champion) { described_class.dd_specific_champion(champion_name: champion_name) }
     let(:champion_name) { 'Aatrox' }
     let(:url) do
       { url: "http://ddragon.leagueoflegends.com/cdn/#{response['n']['profileicon']}"\
